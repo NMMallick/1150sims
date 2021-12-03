251895d56efcea615596960a8fbae849de643ca8
juinsommer <juinchoisommer@gmail.com>
2021-12-03 13:03:47 -0500

    solves for minimum time to avoid accident without total delay from system.py

--- kinematics.py
+++ kinematics.py
@@ -1,10 +1,13 @@
 # Simulation for a 2 car system in the event of a 
 #   probable rear-end collision
 
+# solve for minimum time to avoid collision, take throughput data from system.py, 
+# add to time to avoid collision, output new minimum distance to avoid collision
+
 # Car A - speeding vehicle on trajectory to collision
 # Car B - vehicle infront of speeding car B
 
-import math 
+import math
 
 # 0.8g's (7.84532 m/s^2) is the limit to effective braking of a given vehicle
 acc_limit = 7.84532
@@ -29,4 +32,21 @@ xAB = 2 - ((vel**2)/(2*(-acc_limit)))
 
 print("Minimum distance before braking: (meters) ", xAB)
 print("Minimum distance before braking: (feet) ", xAB*3.281)
+
+#calculate minimum time required to avoid accident given braking distance
+d = (vel**2) + (2*acc_limit*xAB)
+sol1 = (-vel + math.sqrt(abs(d)))/(acc_limit)
+sol2 = (-vel - math.sqrt(abs(d)))/(acc_limit)
+
+if sol1 < 0:
+    time = sol2
+
+elif sol2 < 0:
+    time = sol1
+
+#add total delay data from system.py to time 
+#solve for new minimum distance required to avoid accident
+
+
+print("Minimum time required to avoid accident: (s) ", time)
 
